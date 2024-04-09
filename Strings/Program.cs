ecd12c4e52120dec8b0988b20cf72cf7ab51b62f
mawa17 <mawa17@elev.tec.dk>
2024-04-09 09:48:43 +0200

    Strings

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,21 +1,32 @@
 ﻿/*Må jeg SNART lave må mine EGNE C# projekter?*/
 #region Opgave1
 {
-
+    int _ = default;
+    string __ = default;
+    double ___ = default;
+    Console.WriteLine($"{_}{__}{___+.1}");
 }
 #endregion
 #region Opgave2
 {
-
+    int _ = default;
+    string __ = default;
+    double ___ = default;
+    ___ = 0.2;
+    Console.WriteLine($"{_}{__}{___}");
 }
 #endregion
 #region Opgave3
 {
-
+    string _ = "I dag har vi den 24. December";
+    Console.WriteLine(_);
 }
 #endregion
 #region Opgave4
 {
-
+    var givDenEtNavnOgVærdien = 200.50;
+    var etNavnOgVærdien = "Jeg har";
+    var medEtNavnOgVærdien = " kr. i banken.";
+    Console.WriteLine(etNavnOgVærdien + givDenEtNavnOgVærdien + medEtNavnOgVærdien);
 }
 #endregion
