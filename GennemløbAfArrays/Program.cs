c91f34f850494b773f3c52720e58c45bfdae65e0
mawa17 <163126157+mawa17@users.noreply.github.com>
2024-04-25 10:39:52 +0200

    DONE

--- GennemløbAfArrays/Program.cs
+++ GennemløbAfArrays/Program.cs
@@ -1,7 +1,13 @@
 ﻿/*Må jeg SNART lave må mine EGNE C# projekter?*/
 
+var _ = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+var __ = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 #region Opgave1
 {
-
+    Console.WriteLine(_.Sum());
+	for (int i = 0; i < __.Length; i++)
+	{
+        Console.WriteLine(__[i]);
+    }
 }
 #endregion
