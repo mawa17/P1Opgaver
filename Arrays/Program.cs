4fe1fa75072b40ee8c0d9f32900b733706248873
mawa17 <163126157+mawa17@users.noreply.github.com>
2024-04-26 11:00:55 +0200

    DONE

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -3,6 +3,10 @@
 #region Opgave1
 {
     var a = new int[] { -2, -1, 0, 10 };
-    Console.WriteLine(a[^0]);
+    Console.WriteLine(a[^1]);
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.WriteLine(a[i]);
+    }
 }
 #endregion
--- PublicPrivateProtected/Program.cs
+++ PublicPrivateProtected/Program.cs
@@ -2,6 +2,6 @@
 
 #region Opgave1
 {
-
+https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/access-modifiers#summary-table
 }
 #endregion
